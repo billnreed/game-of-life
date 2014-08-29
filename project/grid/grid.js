928a24789d2d85732bde2423361523b12b573d7d
Bill Reed <billnreed@gmail.com>
2014-08-29 01:02:37 -0700

    added scaffolding for tick and working on the getAliveNeighborCount method

--- project/grid/grid.js
+++ project/grid/grid.js
@@ -1,24 +1,71 @@
-function Grid() {
+function GridDirective() {
     return {
-        restrict: 'E',
-        templateUrl: 'grid/grid-template.html',
-        controller: 'GridCtrl',
+        restrict    : 'E',
+        templateUrl : 'grid/grid-template.html',
+        controller  : 'GridCtrl',
         controllerAs: 'grid'
     };
 }
 
 function GridCtrl(GridService) {
     this.grid = GridService.grid;
+
 }
 
 function GridService() {
-    this.grid = [[{isAlive: false}, {isAlive: false}, {isAlive: false}, {isAlive: false}],
-        [{isAlive: false}, {isAlive: false}, {isAlive: false}, {isAlive: false}],
-        [{isAlive: false}, {isAlive: false}, {isAlive: false}, {isAlive: false}],
-        [{isAlive: false}, {isAlive: false}, {isAlive: false}, {isAlive: false}]];
+    this.grid = [
+        [
+            {isAlive: false},
+            {isAlive: false},
+            {isAlive: false},
+            {isAlive: false}
+        ],
+        [
+            {isAlive: false},
+            {isAlive: false},
+            {isAlive: false},
+            {isAlive: false}
+        ],
+        [
+            {isAlive: false},
+            {isAlive: false},
+            {isAlive: false},
+            {isAlive: false}
+        ],
+        [
+            {isAlive: false},
+            {isAlive: false},
+            {isAlive: false},
+            {isAlive: false}
+        ]
+    ];
+
+    this.tick = function() {
+        var gridCopy = angular.copy(this.grid);
+
+        for (var i = 0; i < this.grid.length; i++) {
+            for (var j = 0; j < this.grid[i].length; j++) {
+                var aliveNeighborCount = this.getAliveNeighborCount(i, j);
+
+            }
+        }
+
+        this.grid = gridCopy;
+    };
+
+    this.getAliveNeighborCount = function(row, col) {
+        return (+this.grid[row - 1][col].isAlive)
+            + (+this.grid[row + 1][col].isAlive)
+            + (+this.grid[row][col - 1].isAlive)
+            + (+this.grid[row][col + 1].isAlive)
+            + (+this.grid[row - 1][col - 1].isAlive)
+            + (+this.grid[row - 1][col + 1].isAlive)
+            + (+this.grid[row + 1][col - 1].isAlive)
+            + (+this.grid[row + 1][col + 1].isAlive);
+    };
 }
 
 angular.module('gameOfLife')
-    .directive('grid', Grid)
+    .directive('grid', GridDirective)
     .controller('GridCtrl', GridCtrl)
     .service('GridService', GridService);
--- test/grid-service.spec.js
+++ test/grid-service.spec.js
@@ -5,9 +5,176 @@ describe('The GridService', function() {
 
     beforeEach(inject(function(GridService) {
         gridService = GridService;
+
+        gridService.grid = [
+            [
+                {isAlive: false},
+                {isAlive: false},
+                {isAlive: false}
+            ],
+            [
+                {isAlive: false},
+                {isAlive: false},
+                {isAlive: false}
+            ],
+            [
+                {isAlive: false},
+                {isAlive: false},
+                {isAlive: false}
+            ]
+        ];
     }));
 
     it('exposes a grid property', function() {
         expect(gridService.grid).toBeDefined();
+    });
+
+    describe('finding the number of alive neighbors', function() {
+        describe('of a center cell', function() {
+            var row = 1;
+            var col = 1;
+
+            it('finds 0 for no alive neighbors', function() {
+                expect(gridService.getAliveNeighborCount(row, col)).toEqual(0);
+            });
+
+            describe('for 1 live neighbor', function() {
+                it('finds 1 for a live neighbor on an edges center', function() {
+                    gridService.grid[row - 1][col].isAlive = true;
+
+                    expect(gridService.getAliveNeighborCount(row, col)).toEqual(1);
+                });
+
+                it('finds 1 for a live neighbor on a diagonal', function() {
+                    gridService.grid[row - 1][col - 1].isAlive = true;
+
+                    expect(gridService.getAliveNeighborCount(row, col)).toEqual(1);
+                });
+            });
+
+            describe('for 2 live neighbors', function() {
+                it('finds 2 for two neighbors on opposite edges', function() {
+                    gridService.grid[row][col - 1].isAlive = true;
+                    gridService.grid[row + 1][col + 1].isAlive = true;
+
+                    expect(gridService.getAliveNeighborCount(row, col)).toEqual(2);
+                });
+                it('finds 2 for two adjacent live neighbors', function() {
+                    gridService.grid[row + 1][col].isAlive = true;
+                    gridService.grid[row + 1][col + 1].isAlive = true;
+
+                    expect(gridService.getAliveNeighborCount(row, col)).toEqual(2);
+                });
+                it('finds 2 for two neighbors on adjacent edges', function() {
+                    gridService.grid[row - 1][col].isAlive = true;
+                    gridService.grid[row][col - 1].isAlive = true;
+
+                    expect(gridService.getAliveNeighborCount(row, col)).toEqual(2);
+                });
+            });
+
+            describe('for 3 live neighbors', function() {
+                it('finds 3 for three live neighbors on one edge', function() {
+                    gridService.grid[row - 1][col - 1].isAlive = true;
+                    gridService.grid[row - 1][col].isAlive = true;
+                    gridService.grid[row - 1][col + 1].isAlive = true;
+
+                    expect(gridService.getAliveNeighborCount(row, col)).toEqual(3);
+                });
+
+                it('finds 3 for one live neighbor on three edges', function() {
+                    gridService.grid[row - 1][col].isAlive = true;
+                    gridService.grid[row + 1][col].isAlive = true;
+                    gridService.grid[row][col - 1].isAlive = true;
+
+                    expect(gridService.getAliveNeighborCount(row, col)).toEqual(3);
+                });
+
+                it('finds 3 for one live two live neighbors on edge and one live neighbor on another', function() {
+                    gridService.grid[row - 1][col - 1].isAlive = true;
+                    gridService.grid[row - 1][col].isAlive = true;
+                    gridService.grid[row + 1][col].isAlive = true;
+
+                    expect(gridService.getAliveNeighborCount(row, col)).toEqual(3);
+                });
+            });
+
+            describe('for 4 live neighbors', function() {
+                it('finds 4 for four live neighbors', function() {
+                    gridService.grid[row - 1][col - 1].isAlive = true;
+                    gridService.grid[row - 1][col + 1].isAlive = true;
+                    gridService.grid[row][col + 1].isAlive = true;
+                    gridService.grid[row - 1][col].isAlive = true;
+
+                    expect(gridService.getAliveNeighborCount(row, col)).toEqual(4);
+                });
+            });
+
+            describe('for 5 live neighbors', function() {
+                it('finds 5 for five live neighbors', function() {
+                    gridService.grid[row - 1][col - 1].isAlive = true;
+                    gridService.grid[row - 1][col].isAlive = true;
+                    gridService.grid[row - 1][col + 1].isAlive = true;
+                    gridService.grid[row + 1][col - 1].isAlive = true;
+                    gridService.grid[row + 1][col].isAlive = true;
+
+                    expect(gridService.getAliveNeighborCount(row, col)).toEqual(5);
+                });
+            });
+
+            describe('for 6 live neighbors', function() {
+                it('finds 6 for six live neighbors', function() {
+                    gridService.grid[row - 1][col - 1].isAlive = true;
+                    gridService.grid[row - 1][col].isAlive = true;
+                    gridService.grid[row - 1][col + 1].isAlive = true;
+                    gridService.grid[row + 1][col - 1].isAlive = true;
+                    gridService.grid[row + 1][col].isAlive = true;
+                    gridService.grid[row + 1][col + 1].isAlive = true;
+
+                    expect(gridService.getAliveNeighborCount(row, col)).toEqual(6);
+                });
+            });
+
+            describe('for 7 live neighbors', function() {
+                it('finds 7 for seven live neighbors', function() {
+                    gridService.grid[row - 1][col - 1].isAlive = true;
+                    gridService.grid[row - 1][col].isAlive = true;
+                    gridService.grid[row - 1][col + 1].isAlive = true;
+                    gridService.grid[row + 1][col - 1].isAlive = true;
+                    gridService.grid[row + 1][col].isAlive = true;
+                    gridService.grid[row + 1][col + 1].isAlive = true;
+                    gridService.grid[row][col - 1].isAlive = true;
+
+                    expect(gridService.getAliveNeighborCount(row, col)).toEqual(7);
+                });
+            });
+
+            describe('for 8 live neighbors', function() {
+                it('finds 8 for eight live neighbors', function() {
+                    gridService.grid[row - 1][col - 1].isAlive = true;
+                    gridService.grid[row - 1][col].isAlive = true;
+                    gridService.grid[row - 1][col + 1].isAlive = true;
+                    gridService.grid[row + 1][col - 1].isAlive = true;
+                    gridService.grid[row + 1][col].isAlive = true;
+                    gridService.grid[row + 1][col + 1].isAlive = true;
+                    gridService.grid[row][col - 1].isAlive = true;
+                    gridService.grid[row][col + 1].isAlive = true;
+
+                    expect(gridService.getAliveNeighborCount(row, col)).toEqual(8);
+                });
+            });
+        });
+
+        describe('of an edge cell', function() {
+
+            describe('on the top', function() {
+                var row = 0;
+                var col = 1;
+
+                it('finds 0 for no alive neighbors', function() {
+                    expect(gridService.getAliveNeighborCount(row, col)).toEqual(0);
+                });
+            });
+        });
     });
 });
